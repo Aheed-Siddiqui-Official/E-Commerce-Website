891f4d7ad654280f5923ebffbf820efba8338572
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-04-16 08:00:40 +0500

    commit

---
+++ src/updateCartProductTotal.js
@@ -0,0 +1,17 @@
+import { getCartProductFromLS } from "./getCartProducts";
+
+export const updateCartProductTotal = () => {
+    let productSubTotal = document.querySelector(".productSubTotal");
+    let productFinalTotal = document.querySelector(".productFinalTotal");
+
+    let localCartProducts = getCartProductFromLS();
+    let initialValue = 0;
+    let totalProductPrice = localCartProducts.reduce((accum, currElem) => {
+        let productPrice = parseInt(currElem.price) || 0 ;
+        return accum + productPrice;
+    }, initialValue);
+    // console.log(totalProductPrice);
+    
+    productSubTotal.textContent = `Rs:${totalProductPrice}`;
+    productFinalTotal.textContent = `Rs:${totalProductPrice + 50}`;
+} 
